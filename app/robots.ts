daa44f4cf602672d250a69659d738fda980287fb
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2025-06-26 19:06:28 +0500

    Revert "refactor: hide website from search"
    This reverts commit b08b1e4b04fbf94a0d49892c55f1276e9322133e.

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/', '/user/*']
+      disallow: '/user/*'
     },
     sitemap: `${BASE_URL}/sitemap.xml`
   };
