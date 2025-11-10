3b0d2d04b309687b5f4c2f99984653df290b6f59
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2025-11-10 22:49:55 +0500

    remove not completed services

--- components/layout/AppHeader/components/HeaderTop/HeaderTop.tsx
+++ components/layout/AppHeader/components/HeaderTop/HeaderTop.tsx
@@ -1,9 +1,8 @@
-import { ChevronDownIcon, MapPinIcon, ScaleIcon } from 'lucide-react';
+import { ChevronDownIcon, MapPinIcon } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
 import { BaseLayout } from '@/components/layout';
-import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,7 +10,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { Link } from '@/i18n/navigation';
 
 import { LocaleSwitcher } from './components';
@@ -38,22 +36,22 @@ export const HeaderTop = () => {
               <Link href='/payment-methods'>
                 <DropdownMenuItem>{t('Payment methods')}</DropdownMenuItem>
               </Link>
-              <Tooltip delayDuration={0}>
-                <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>
-                  <TooltipTrigger disabled className='w-full'>
-                    {t('Delivery by transport company')}
-                  </TooltipTrigger>
-                </DropdownMenuItem>
-                <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>
-              </Tooltip>
-              <Tooltip delayDuration={0}>
-                <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>
-                  <TooltipTrigger disabled className='w-full'>
-                    {t('Pickup')}
-                  </TooltipTrigger>
-                </DropdownMenuItem>
-                <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>
-              </Tooltip>
+              {/*<Tooltip delayDuration={0}>*/}
+              {/*  <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>*/}
+              {/*    <TooltipTrigger disabled className='w-full'>*/}
+              {/*      {t('Delivery by transport company')}*/}
+              {/*    </TooltipTrigger>*/}
+              {/*  </DropdownMenuItem>*/}
+              {/*  <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>*/}
+              {/*</Tooltip>*/}
+              {/*<Tooltip delayDuration={0}>*/}
+              {/*  <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>*/}
+              {/*    <TooltipTrigger disabled className='w-full'>*/}
+              {/*      {t('Pickup')}*/}
+              {/*    </TooltipTrigger>*/}
+              {/*  </DropdownMenuItem>*/}
+              {/*  <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>*/}
+              {/*</Tooltip>*/}
             </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
@@ -67,29 +65,29 @@ export const HeaderTop = () => {
               <Link href='/about'>
                 <DropdownMenuItem>{t('About company')}</DropdownMenuItem>
               </Link>
-              <Tooltip delayDuration={0}>
-                <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>
-                  <TooltipTrigger disabled className='w-full'>
-                    {t('Information for investors')}
-                  </TooltipTrigger>
-                </DropdownMenuItem>
-                <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>
-              </Tooltip>
+              {/*<Tooltip delayDuration={0}>*/}
+              {/*  <DropdownMenuItem asChild className='text-muted-foreground cursor-not-allowed'>*/}
+              {/*    <TooltipTrigger disabled className='w-full'>*/}
+              {/*      {t('Information for investors')}*/}
+              {/*    </TooltipTrigger>*/}
+              {/*  </DropdownMenuItem>*/}
+              {/*  <TooltipContent side='right'>{t('In the process of development')}</TooltipContent>*/}
+              {/*</Tooltip>*/}
               <Link href='/requisites'>
                 <DropdownMenuItem>{t('Requisites')}</DropdownMenuItem>
               </Link>
             </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
-        <Tooltip delayDuration={0}>
-          <TooltipTrigger asChild>
-            <Button className='h-6 px-2' size='sm' variant='secondaryFlat'>
-              <ScaleIcon />
-              {t('Buy as a legal entity')}
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side='bottom'>{t('In the process of development')}</TooltipContent>
-        </Tooltip>
+        {/*<Tooltip delayDuration={0}>*/}
+        {/*  <TooltipTrigger asChild>*/}
+        {/*    <Button className='h-6 px-2' size='sm' variant='secondaryFlat'>*/}
+        {/*      <ScaleIcon />*/}
+        {/*      {t('Buy as a legal entity')}*/}
+        {/*    </Button>*/}
+        {/*  </TooltipTrigger>*/}
+        {/*  <TooltipContent side='bottom'>{t('In the process of development')}</TooltipContent>*/}
+        {/*</Tooltip>*/}
       </div>
       <div className='flex items-center gap-4'>
         <LocaleSwitcher />
