0f0992fe9e02e5d476dbeb0a25e8cb9022e6560c
abbosbek <hikmatullaevabbos24@gmail.com>
2026-02-13 08:39:05 +0500

    request path to api of promocodes list changed

--- utils/api/requests/promos/index.ts
+++ utils/api/requests/promos/index.ts
@@ -1,7 +1,7 @@
 import { api } from '@/utils/api/instance';
 
 export const getPromos = (requestConfig?: RequestConfig) =>
-  api.get<PromosResponse>('/promocodes/', requestConfig?.config);
+  api.get<PromosResponse>('/base/promocodes/', requestConfig?.config);
 
 export const postPromoCodeChecker = ({ data, config }: RequestConfig<{ promocode: string }>) =>
   api.post<PromoCodeCheckerResponse>('/base/promocode/checker/', data, config);
